a4ebb27891952ef6523c8ca3d5d30a52fb19a551
yalishanda <farbantis@gmail.com>
2023-07-09 15:59:09 +0100

    finished settings

---
+++ girosmani/settings/development.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+INTERNAL_IPS = ["127.0.0.1"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
+
