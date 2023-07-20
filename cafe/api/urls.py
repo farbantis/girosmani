5cf951a8adc2095792b539bcd638315f1580915c
yalishanda <farbantis@gmail.com>
2023-07-20 15:34:30 +0100

    adding API for header menu +path

--- cafe/api/urls.py
+++ cafe/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework import routers
-from cafe.api.resources import MenuListAPIView, OrderAPIVIewSet, OrderItemAPIVIewSet
+from cafe.api.resources import MenuListAPIView, OrderAPIVIewSet, OrderItemAPIVIewSet, HeaderMenuAPIView
 
 router = routers.SimpleRouter()
 router.register(r'order', OrderAPIVIewSet)
@@ -10,5 +10,6 @@ urlpatterns = router.urls
 urlpatterns += [
     path('menu/', MenuListAPIView.as_view()),
     path('menu/<slug:group>/', MenuListAPIView.as_view()),
+    path('header_menu/', HeaderMenuAPIView.as_view()),
     # path('order/', OrderAPIVIew.as_view().as_view()),
 ]
