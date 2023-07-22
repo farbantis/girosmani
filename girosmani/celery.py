61ca1a532b347c38e898a32fda53b0da2802e284
yalishanda <farbantis@gmail.com>
2023-07-22 13:21:02 +0100

    improving celery

--- girosmani/celery.py
+++ girosmani/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 from girosmani.settings import CELERY_BROKER_URL
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'girosmani.settings')
-app = Celery('girosmani', broker=CELERY_BROKER_URL, backend=CELERY_BROKER_URL)
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery('girosmani')
+app.config_from_object('django.conf:settings', namespace='CELERY')  # from settings will load backend
 app.autodiscover_tasks()  # can we indicate specific apps insted of all: lambda: settings.INSTALLED_APPS
 
--- girosmani/settings/base.py
+++ girosmani/settings/base.py
@@ -158,5 +158,5 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 # API token kill after
 FORCE_KILL_TOKEN = 6_000_000
 
-CELERY_BROKER_URL = 'redis://localhost:6379'  # Use Redis on the local machine
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'  # Use Redis on the local machine
+CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Use Redis on the local machine
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'  # Use Redis on the local machine
