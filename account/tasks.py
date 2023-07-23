96872fe2c989c4f87488259cfe77d0033f6998b0
yalishanda <farbantis@gmail.com>
2023-07-23 10:17:26 +0100

    testing html emails success

--- account/tasks.py
+++ account/tasks.py
@@ -1,23 +1,6 @@
 from celery import shared_task
-from django.core.mail import send_mail
 import os
-
-
-# @shared_task(expires=3600)  # time to execute - 1 hour
-# def new_user_email_notification(user):
-#     email = 'farbantis@gmail.com' #user.user.email
-#     subject = f'registration confirmation'
-#     message = f"""
-#         Dear {email},
-#         you successfully registered with pirosmani
-#         your status is bronze
-#     """
-#     # {user.status} is no available here?.
-#     from_email = os.getenv('EMAIL_HOST_USER')
-#     recipient_list = ['yalisanda@gmail.com']
-#     send_mail(subject, message, from_email, recipient_list, fail_silently=False)
-
-from django.core.mail import EmailMultiAlternatives
+from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
 
@@ -27,19 +10,14 @@ def new_user_email_notification(user):
     context = {
         'username': user,
         'cafe_name': 'Girosmani Cafe',
-        'order_url': 'https://qimeer.online/account/registration_confirmation_email/',
+        'order_url': 'https://qimeer.online/cafe/',
     }
     html_content = render_to_string('account/letter_registration_confirmation.html', context)
     from_email = os.getenv('EMAIL_HOST_USER')
     subject = 'Welcome to Girosmani Cafe - Registration Confirmation'
-    email = EmailMultiAlternatives(subject, from_email, [recipient_email])
-    email.attach_alternative(html_content, "text/html")
-    email.send()
-
-    message = 'ordinary email' + html_content + subject
     send_mail(
         subject=subject,
-        message='hello message',
+        message='Welcome to Girosmani',
         html_message=html_content,
         from_email=from_email,
         recipient_list=[recipient_email],
